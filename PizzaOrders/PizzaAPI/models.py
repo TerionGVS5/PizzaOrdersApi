5f7a42d4f26a948df33f552a52570153a239f80e
TerionGVS5 <Vveterokk@gmail.com>
2018-05-23 14:45:04 +1000

    Add Pizza Order model

--- PizzaOrders/PizzaAPI/models.py
+++ PizzaOrders/PizzaAPI/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid as uuid_lib
+
 
 # Create your models here.
+class OrderPizza(models.Model):
+    size_30cm = '30cm'
+    size_50cm = '50cm'
+    sizes = (
+        (size_30cm, '30cm'),
+        (size_50cm, '50cm'),
+    )
+    pizza_size = models.CharField(
+        max_length=4,
+        choices=sizes,
+        default=size_30cm,
+    )
+    customer_name = models.CharField(max_length=100)
+    customer_address = models.CharField(max_length=300)
+    uuid = models.UUIDField(  # Used by the API to look up the record
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False)
