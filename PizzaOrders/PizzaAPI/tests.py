856e1cbc46c551c0838b7730c2e3249ff3e22a97
TerionGVS5 <Vveterokk@gmail.com>
2018-05-23 16:25:27 +1000

    Added tests for get and delete Pizza Orders

--- PizzaOrders/PizzaAPI/tests.py
+++ PizzaOrders/PizzaAPI/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from .models import OrderPizza
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+
+class TestOrderPizzaModel:
+
+    def test_get_saved(self):
+        c = Client()
+        new_order_pizza = OrderPizza.objects.create(
+            pizza_size="30cm",
+            customer_name="Jonn",
+            customer_address="Vladivostok"
+
+        )
+        response = c.get('/api/{}/'.format(new_order_pizza.uuid))
+        assert response.status_code == 200
+
+        assert response.json()['customer_name'] == "Jonn"
+
+    def test_delete(self):
+        c = Client()
+        new_order_pizza = OrderPizza.objects.create(
+            pizza_size="30cm",
+            customer_name="Jonn",
+            customer_address="Vladivostok"
+
+        )
+        response = c.delete('/api/{}/'.format(new_order_pizza.uuid))
+        assert OrderPizza.objects.all().count() == 0
